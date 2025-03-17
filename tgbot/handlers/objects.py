b1166ff8c000ff1d159e1de8dbc2e0b1a14ff2c5
МС ЧЕХНИК <petrichenko8502@gmail.com>
2025-03-17 17:28:33 +0400

    removed sending files

--- tgbot/handlers/objects.py
+++ tgbot/handlers/objects.py
@@ -2,9 +2,8 @@ import json
 import logging
 from typing import List, Dict
 
-from aiogram import Router, html, F, exceptions
-from aiogram.client.default import Default
-from aiogram.types import BufferedInputFile, Message
+from aiogram import Router, html, F
+from aiogram.types import Message
 
 from tgbot.keyboards import reply
 
@@ -19,24 +18,6 @@ SHARED_TITLES_TYPES = {
 }
 
 
-def get_safe_message_dict(message: Dict) -> Dict:
-    def unpack(obj: Dict):
-        temp_dict = {}
-
-        for key, value in obj.items():
-            if not isinstance(value, dict):
-                if isinstance(value, Default):
-                    temp_dict[key] = value.name
-                else:
-                    temp_dict[key] = value
-            else:
-                temp_dict[key] = unpack(value)
-
-        return temp_dict
-
-    return unpack(message)
-
-
 def create_caption(object_info: Dict, important_info: List[List] = None) -> tuple:
     important_caption = ''
 
@@ -49,53 +30,13 @@ def create_caption(object_info: Dict, important_info: List[List] = None) -> tupl
 
 
 async def send_object_info(message: Message, important_info: List[List] = None):
-    message_dict = get_safe_message_dict(message.model_dump())
-    file = BufferedInputFile(json.dumps(message_dict).encode(), filename=f'{message.content_type.value}.json')
-
-    object_info = message_dict.get(message.content_type)
+    object_info = message.model_dump().get(message.content_type)
     caption, important_caption = create_caption(object_info, important_info)
 
-    try:
-        await message.reply_document(
-            document=file,
-            caption=important_caption + caption,
-            reply_markup=reply.get_main_keyboard()
-        )
-
-    except exceptions.TelegramBadRequest:
-        await message.reply_document(
-            document=file,
-            caption=important_caption,
-            reply_markup=reply.get_main_keyboard()
-        )
-
-
-@router.message(F.sticker)
-async def get_sticker_object(message: Message):
-    important_info = [[html.bold('👁 File_ID'), html.code(message.sticker.file_id)]]
-
-    await send_object_info(message=message, important_info=important_info)
-
-
-@router.message(F.photo)
-async def get_photo_object(message: Message):
-    important_info = [[html.bold('👁 File_ID'), html.code(message.photo[-1].file_id)]]
-
-    await send_object_info(message=message, important_info=important_info)
-
-
-@router.message(F.video)
-async def get_video_object(message: Message):
-    important_info = [[html.bold('👁 File_ID'), html.code(message.video.file_id)]]
-
-    await send_object_info(message=message, important_info=important_info)
-
-
-@router.message(F.animation)
-async def get_animation_object(message: Message):
-    important_info = [[html.bold('👁 File_ID'), html.code(message.animation.file_id)]]
-
-    await send_object_info(message=message, important_info=important_info)
+    await message.reply(
+        text=important_caption + caption,
+        reply_markup=reply.get_main_keyboard()
+    )
 
 
 @router.message(F.users_shared)
@@ -124,12 +65,37 @@ async def get_chat_object(message: Message):
 
 @router.message()
 async def get_other_objects(message: Message):
-    important_info = [
-        [html.bold('👁 BOT HTML'), html.code(html.quote(message.html_text))],
-        [
-            html.bold('👁 USER HTML'),
-            html.code(html.quote(message.html_text.replace('tg-emoji', 'emoji').replace('emoji-id', 'id')))
+    if message.photo:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.photo[-1].file_id)]]
+
+    elif message.video:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.video.file_id)]]
+
+    elif message.document:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.document.file_id)]]
+
+    elif message.audio:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.audio.file_id)]]
+
+    elif message.voice:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.voice.file_id)]]
+
+    elif message.video_note:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.video_note.file_id)]]
+
+    elif message.animation:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.animation.file_id)]]
+
+    elif message.sticker:
+        important_info = [[html.bold('👁 File_ID'), html.code(message.sticker.file_id)]]
+
+    else:
+        important_info = [
+            [html.bold('👁 BOT HTML'), html.code(html.quote(message.html_text))],
+            [
+                html.bold('👁 USER HTML'),
+                html.code(html.quote(message.html_text.replace('tg-emoji', 'emoji').replace('emoji-id', 'id')))
+            ]
         ]
-    ]
 
     await send_object_info(message=message, important_info=important_info)
